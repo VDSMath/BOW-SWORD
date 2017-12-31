6930ada192cc52b591e18766d3e5cf90d6998fd9
VDSMath <matheusvdsdf@gmail.com>
2017-12-31 02:34:57 -0200

    Base Attack, New Prefabs, Splitscreen

--- Assets/Scripts/PlayerBaseAttack.cs
+++ Assets/Scripts/PlayerBaseAttack.cs
@@ -2,51 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CharType
-{
-    Knight, Archer
-}
-
-public enum CharName
-{
-    Gus, Tarzan
-}
-
-public class PlayerBaseAttack : MonoBehaviour {
-
-    public CharType Class;
-
-    [Header("Attack Parameters")]
-    [SerializeField]
-    private float damage;
-    [SerializeField]
-    private GameObject sword;
-    [SerializeField]
-    private float AttackRayLength = 2f;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-    }
-
-    void Attack()
-    {
-        sword.GetComponent<Animator>().SetTrigger("attack");
-
-        Vector3 camForward = GetComponent<PlayerMovement>().cam.transform.forward;
-        RaycastHit hit;
-
-        if (Physics.Raycast(GetComponent<PlayerMovement>().cam.transform.position, camForward, out hit, AttackRayLength))
-        {
-            if (hit.collider.gameObject.tag == "Attackable")
-            {
-                StartCoroutine(hit.collider.gameObject.GetComponent<Attackable>().Attacked());
-            }
-        }
-    }
+public interface PlayerBaseAttack {
+    void Skill1();
+    void Skill2();
+    void Special();
 }
